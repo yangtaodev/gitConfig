a88eba509e26103c5b014072e9cc2eea02c0fa8a
Yigit <30613559+yangtaodev@users.noreply.github.com>
2022-04-05 14:20:11 +0800

    Create init.sh

--- init.sh
+++ init.sh
@@ -6,7 +6,7 @@ cd $base_dir
 cur_dir=`pwd`
 chmod 777 commit-msg
 git config commit.template $cur_dir$temp_dir
-git config core.hooksPath base_dir
+git config core.hooksPath $base_dir
 echo "小伙子很有觉悟,加油！💚"
 cd -
 
